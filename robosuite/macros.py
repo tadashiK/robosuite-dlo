774c78be5a814b0626da893ba2291d0f19a3418c
Tadashi Kozuno <tadashi.kozuno@gmail.com>
2023-04-02 04:09:52 +0900

    bug fix: importing custom macros when a custom macro file exists (cf https://github.com/ARISE-Initiative/robosuite/issues/385)

--- robosuite/macros.py
+++ robosuite/macros.py
@@ -7,38 +7,50 @@ To make sure global reference is maintained, should import these settings as:
 `import robosuite.macros as macros`
 """
 
-# Global Mujoco Simulation Parameters
-SIMULATION_TIMESTEP = 0.002  # Internal simulation timestep (in seconds)
+import os
 
-# Instance Randomization
-# Used if we want to randomize geom groups uniformly per instance -- e.g.: entire robot arm, vs. per-joint geom
-# This should get set to True in your script BEFORE an environment is created or the DR wrapper is used
-USING_INSTANCE_RANDOMIZATION = False
+import robosuite
 
-# Numba settings
-# TODO: Numba causes BSOD for NutAssembly task when rendering offscreen (deterministically!)
-ENABLE_NUMBA = True
-CACHE_NUMBA = True
+# Check if macros_private.py exists.
+base_path = robosuite.__path__[0]
+macros_private_path = os.path.join(base_path, "macros_private.py")
 
-# Image Convention
-# Robosuite (Mujoco)-rendered images are based on the OpenGL coordinate frame convention, whereas many downstream
-# applications assume an OpenCV coordinate frame convention. For consistency, you can set the image convention
-# here; this will assure that any rendered frames will match the associated convention.
-# See the figure at the bottom of https://amytabb.com/ts/2019_06_28/ for an informative overview.
-IMAGE_CONVENTION = "opengl"  # Options are {"opengl", "opencv"}
+if os.path.exists(macros_private_path) and __file__ != macros_private_path:
+    from robosuite.macros_private import *
+else:
+    # Modify the following variables in macros_private.py
+    # Global Mujoco Simulation Parameters
+    SIMULATION_TIMESTEP = 0.002  # Internal simulation timestep (in seconds)
 
-# Image concatenation
-# In general, observations are concatenated together by modality. However, image observations are expensive memory-wise,
-# so we skip concatenating all images together by default, unless this flag is set to True
-CONCATENATE_IMAGES = False
+    # Instance Randomization
+    # Used if we want to randomize geom groups uniformly per instance -- e.g.: entire robot arm, vs. per-joint geom
+    # This should get set to True in your script BEFORE an environment is created or the DR wrapper is used
+    USING_INSTANCE_RANDOMIZATION = False
 
-MUJOCO_GPU_RENDERING = True
+    # Numba settings
+    # TODO: Numba causes BSOD for NutAssembly task when rendering offscreen (deterministically!)
+    ENABLE_NUMBA = True
+    CACHE_NUMBA = True
 
-# Spacemouse settings. Used by SpaceMouse class in robosuite/devices/spacemouse.py
-SPACEMOUSE_VENDOR_ID = 9583
-SPACEMOUSE_PRODUCT_ID = 50735
+    # Image Convention
+    # Robosuite (Mujoco)-rendered images are based on the OpenGL coordinate frame convention, whereas many downstream
+    # applications assume an OpenCV coordinate frame convention. For consistency, you can set the image convention
+    # here; this will assure that any rendered frames will match the associated convention.
+    # See the figure at the bottom of https://amytabb.com/ts/2019_06_28/ for an informative overview.
+    IMAGE_CONVENTION = "opengl"  # Options are {"opengl", "opencv"}
 
-# If LOGGING LEVEL is set to None, the logger will be turned off
-CONSOLE_LOGGING_LEVEL = "WARN"
-# File logging is written to /tmp/robosuite.log by default
-FILE_LOGGING_LEVEL = "DEBUG"
+    # Image concatenation
+    # In general, observations are concatenated together by modality. However, image observations are expensive memory-wise,
+    # so we skip concatenating all images together by default, unless this flag is set to True
+    CONCATENATE_IMAGES = False
+
+    MUJOCO_GPU_RENDERING = True
+
+    # Spacemouse settings. Used by SpaceMouse class in robosuite/devices/spacemouse.py
+    SPACEMOUSE_VENDOR_ID = 9583
+    SPACEMOUSE_PRODUCT_ID = 50735
+
+    # If LOGGING LEVEL is set to None, the logger will be turned off
+    CONSOLE_LOGGING_LEVEL = "WARN"
+    # File logging is written to /tmp/robosuite.log by default
+    FILE_LOGGING_LEVEL = "DEBUG"
